5d430ae0e9599e864429ccf78f5e862375fb3862
yanglongfei <283598349@qq.com>
2019-03-08 12:43:06 +0800

    添加readme和sh

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pptp
 Ubuntu pptp搭建脚本
+* 修改账号密码 /etc/ppp/chap-secrets
+* 修改网卡 iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+apt-get update
+
+apt-get -y install pptpd || {
+  echo "Could not install pptpd" 
+  exit 1
+}
+
+#ubuntu末行exit 0
+sed -i '/^exit 0/d' /etc/rc.local
+
+cat >> /etc/rc.local << END
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+
+iptables -I INPUT -p tcp --dport 22 -j ACCEPT
+
+
+iptables -I INPUT -p tcp --dport 1723 -j ACCEPT
+
+
+iptables -I INPUT  --protocol 47 -j ACCEPT
+
+iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
+
+
+
+iptables -I FORWARD -s 192.168.2.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+
+service pptpd restart
+END
+sh /etc/rc.local
+
+
+cat >/etc/ppp/chap-secrets <<END
+
+vpn pptpd dd123456 *
+END
+cat >/etc/pptpd.conf <<END
+option /etc/ppp/options.pptpd
+logwtmp
+localip 192.168.2.1
+remoteip 192.168.2.10-100
+END
+cat >/etc/ppp/options.pptpd <<END
+name pptpd
+refuse-pap
+refuse-chap
+refuse-mschap
+require-mschap-v2
+require-mppe-128
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
+proxyarp
+lock
+nobsdcomp 
+novj
+novjccomp
+nologfd
+END
+
+
+echo   "INSTALL FINISH"
+echo   "============================================================"
+sleep 2
+
+service pptpd restart
+
+exit 0
