3351b0063a86c3e413656ea45e8d960e67f07523
YangLongFei <283598349@qq.com>
2019-03-12 17:57:12 +0800

    添加注释修改readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# pptp
-Ubuntu pptp搭建脚本
-* 修改账号密码 /etc/ppp/chap-secrets
-* 修改网卡 iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
+# pptp-vpn
+Ubuntu pptp vpn搭建脚本
+* 如果无法一键安装请编辑sh文件，查看注释进行调整
+  * 如果源有问题的同学，请参考https://github.com/JackYang500/ubuntu-aliyun-sources
+  * 修改账号密码 /etc/ppp/chap-secrets
+  * 修改网卡eth0为真实网卡 iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
+  
+# Wechat
+ylf283598349
+# 注意
+* 本教程仅供学习测试用途。
+* 如有问题，可联系作者一起交流。
--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+## 更新源,如果源有问题的同学，请参考https://github.com/JackYang500/ubuntu-aliyun-sources
+sudo apt-get update
 
-apt-get update
-
-apt-get -y install pptpd || {
-  echo "Could not install pptpd" 
+## 安装PPTP服务
+sudo apt-get -y install pptpd || {
+  echo "无法定位软件，请检查源或依赖"
   exit 1
 }
 
-#ubuntu末行exit 0
+## ubuntu rc.local文件末行exit 0
 sed -i '/^exit 0/d' /etc/rc.local
 
+## 添加到开机启动项，并且设置iptables，注意网卡（eth0，ipconfig可查看网卡）
 cat >> /etc/rc.local << END
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
@@ -24,15 +26,14 @@ iptables -I INPUT  --protocol 47 -j ACCEPT
 
 iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
 
-
-
 iptables -I FORWARD -s 192.168.2.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 
 service pptpd restart
 END
+
 sh /etc/rc.local
 
-
+## vpn账号密码配置
 cat >/etc/ppp/chap-secrets <<END
 
 vpn pptpd dd123456 *
@@ -61,8 +62,7 @@ nologfd
 END
 
 
-echo   "INSTALL FINISH"
-echo   "============================================================"
+echo   "安装完成"
 sleep 2
 
 service pptpd restart
